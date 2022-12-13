8f3dd4deab12e755a81ac7d4eb4a6880712b25c3
Tom_48_97 ☠ <1498046+tom4897@users.noreply.github.com>
2022-12-13 12:08:50 +0100

    Fixed and tweaked Sample New Prop
    Fixed: Sample New Prop editable entities ghost previews in Game Master
    Tweaked: Cleaned up and optimized the drawer actions used by New Prop
    Fixed: Destroyed variants of rugged desk wings were missing correct layer presets and materials
    Tweaked: Added snap points to the lid and wing of Sample New Prop
    Fixed: Some of the Sample New Prop assets were missing correct localization or had incorrect strings

--- SampleMod_NewProp/Scripts/Game/UserActions/SCR_DrawerUserAction.c
+++ SampleMod_NewProp/Scripts/Game/UserActions/SCR_DrawerUserAction.c
@@ -2,9 +2,8 @@ class SCR_DrawerUserAction : ScriptedUserAction
 {
 	[Attribute( defvalue: "Drawer 1", uiwidget: UIWidgets.EditBox, desc: "Signal index of drawer" )]
 	private string m_sSignalName;	//for pairing with the user action
-	private int m_iSignalIndex;
+	private int m_iSignalIndex = -1;
 	
-	private IEntity m_Owner;
 	const float MAX_SIGNAL_VALUE = 0.25;
 
 	//! Signal manager to pass signals into proc anim
@@ -24,8 +23,9 @@ class SCR_DrawerUserAction : ScriptedUserAction
 		if(!m_SignalsManager)
 			return false;
 		
-		float signalValue = m_SignalsManager.GetSignalValue(m_iSignalIndex);
-		if(signalValue > 0 && signalValue < MAX_SIGNAL_VALUE*0.9) {
+		if (m_iSignalIndex < 0)
+		{
+			m_iSignalIndex = m_SignalsManager.FindSignal(m_sSignalName);
 			return false;
 		}
 		
@@ -35,11 +35,9 @@ class SCR_DrawerUserAction : ScriptedUserAction
 	//---------------------------------------------------------
 	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
 	{
-		GenericEntity genericUserEntity = GenericEntity.Cast(pUserEntity);
-		m_iSignalIndex = m_SignalsManager.FindSignal(m_sSignalName);
 		float targetValue = Math.AbsFloat((m_SignalsManager.GetSignalValue(m_iSignalIndex)) - MAX_SIGNAL_VALUE);
-		
 		m_SignalsManager.SetSignalValue(m_iSignalIndex, targetValue);
+		
 		// Play sound
 		if (m_SoundComponent)
 		{
@@ -52,16 +50,16 @@ class SCR_DrawerUserAction : ScriptedUserAction
 				m_SoundComponent.SoundEvent("SOUND_DRAWER_CLOSE");
 			}
 		}
-		pOwnerEntity.SetFlags(EntityFlags.ACTIVE, true);
 	}
 
 	//------------------------------------------------------------------------------------------------
 	override bool GetActionNameScript(out string outName)
-	{
+	{	
 		if((m_SignalsManager.GetSignalValue(m_iSignalIndex)) == 0)
 		{
-			outName = "Open";
-		}else{
+			outName = "Open"; 
+		} else 
+		{ 
 			outName = "Close";
 		}
 		return true;
@@ -70,17 +68,7 @@ class SCR_DrawerUserAction : ScriptedUserAction
 	//------------------------------------------------------------------------------------------------
 	override void Init(IEntity pOwnerEntity, GenericComponent pManagerComponent)
 	{
-		m_Owner = pOwnerEntity;
-
-		GenericEntity genericEntity = GenericEntity.Cast( m_Owner );
-		if (!genericEntity)
-			return;
-
-		GenericComponent genComponent = GenericComponent.Cast(genericEntity.FindComponent(SignalsManagerComponent));
-		if (!genComponent)
-			return;
-
-		m_SignalsManager = SignalsManagerComponent.Cast(genericEntity.FindComponent(SignalsManagerComponent));
-		m_SoundComponent = SoundComponent.Cast(genericEntity.FindComponent(SoundComponent));
+		m_SignalsManager = SignalsManagerComponent.Cast(pOwnerEntity.FindComponent(SignalsManagerComponent));
+		m_SoundComponent = SoundComponent.Cast(pOwnerEntity.FindComponent(SoundComponent));
 	}
 }
