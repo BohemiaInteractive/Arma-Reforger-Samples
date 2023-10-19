d52b521f0534c1342d64b4dfb54da9cbb8ecc1e1
unknown <a4bldr@BLD-AR-W1-CZUPC.msad.bistudio.com>
2023-10-19 04:21:19 -0700

    Tweaked: Removed obsolete/outdated params
    Tweaked: Updated supply prices of weapons and accessories in Sample New Weapon mod
    
    Tweaked: Damage configuration of Modded Car
    
    Tweaked: Removed & replaced obsolete components
    
    Tweaked: Updated mine trigger collider to use more precise mesh and new MineTrigger layer preset
    
    Fixed: Updated sound project of Sample New Car so engine sounds are working once again with 1.0.0 version
    
    Fixed: Updated animation workspace of Sample New Car so it is now using up to date animation graph & general configuration.
    
    Fixed: Get in & out animations on Sample New Car. Issue was solved by assigning empty animations (v_SampleCar_01_empty.anm) to Player Animation Instance of Sample New Car to Get In, Out & L/R slots.
    
    Changed: Configured transportation of resources on Sample Car.
    
    Tweaked: Damage configuration of Sample New Car
    
    Tweaked: Added action & animation to open trunk on Sample New Car
    
    Tweaked: Connected glass prefabs to bone on Sample New Car so they are moving with i.e. doors or trunk.
    
    Changed: Updated Conflict template used in Sample New Faction
    
    Fixed: Sample New Faction uniforms were swapping back to regular variants when armored vest was worn. This was fixed by adding new deflated prefabs, using similar logic as Worn variant of uniforms retextures

---
+++ SampleMod_NewCar/Assets/Vehicles/Wheeled/SampleCar_01/workspaces/SampleCar_01.aw
@@ -0,0 +1,22 @@
+BaseSource {
+ AnimSetTemplate "{1A2F06EF780C4C79}Assets/Vehicles/Wheeled/SampleCar_01/workspaces/SampleCar_01.ast"
+ AnimSetInstances {
+  "{D281B7A3F44B4B0A}Assets/Vehicles/Wheeled/SampleCar_01/workspaces/SampleCar_01_Player.asi"
+  "{7B6FE5B98D0852F6}Assets/Vehicles/Wheeled/SampleCar_01/workspaces/SampleCar_01_Vehicle.asi"
+ }
+ AnimGraph "{08DD60EF4FC9988A}Assets/Vehicles/Wheeled/SampleCar_01/workspaces/SampleCar_01.agr"
+ PreviewModels {
+  AnimSrcWorkspacePreviewModel "{563EF338E13AB791}" {
+   Model "{790CB9C809DE64B8}Assets/Characters/Animation/AnimTestChar_US_01.xob"
+  }
+  AnimSrcWorkspacePreviewModel "{563EF338E13AB792}" {
+   Model "{B80903BACFCDCCB6}Assets/Vehicles/Wheeled/UAZ469/UAZ469_base.xob"
+  }
+ }
+ AudioTesting AnimSrcWorkspaceAudioTesting "{58B2B32789FAA7C3}" {
+ }
+ AttachmentTesting AnimSrcWorkspaceAttachmentTesting "{524EB33AA33A9F49}" {
+ }
+ IkTesting AnimSrcWorkspaceIkTesting "{52C4E32EA69C4649}" {
+ }
+}
---
+++ SampleMod_NewCar/Scripts/Game/UserActions/SCR_CarTrunkUserAction.c
@@ -0,0 +1,77 @@
+class SCR_TrunkUserAction : ScriptedUserAction
+{
+	[Attribute( defvalue: "Drawer 1", uiwidget: UIWidgets.EditBox, desc: "Signal name of trunk action in procedural animation project" )]
+	protected string m_sSignalName;	//for pairing with the user action
+	[Attribute( defvalue: "0", uiwidget: UIWidgets.EditBox, desc: "Value of signal when trunk is closed" )]
+	protected float m_fSignalMin;
+	[Attribute( defvalue: "1", uiwidget: UIWidgets.EditBox, desc: "Value of signal when trunk is opened" )]
+	protected float m_fSignalMax;
+
+	protected int m_iSignalIndex = -1;
+
+	//! Signal manager to pass signals into proc anim
+	private SignalsManagerComponent m_SignalsManager;
+	//! Sound component
+	private SoundComponent m_SoundComponent;
+
+	//------------------------------------------------------------------------------------------------
+	override bool CanBeShownScript(IEntity user)
+	{
+		return CanBePerformedScript(user);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override bool CanBePerformedScript(IEntity user)
+	{
+		if(!m_SignalsManager)
+			return false;
+
+		if (m_iSignalIndex < 0)
+		{
+			m_iSignalIndex = m_SignalsManager.FindSignal(m_sSignalName);
+			return false;
+		}
+
+		return true;
+	}
+
+	//---------------------------------------------------------
+	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
+	{
+		float targetValue = Math.AbsFloat((m_SignalsManager.GetSignalValue(m_iSignalIndex)) - m_fSignalMax);
+		m_SignalsManager.SetSignalValue(m_iSignalIndex, targetValue);
+
+		// Play sound
+		if (m_SoundComponent)
+		{
+			if(targetValue > 0)
+			{
+				m_SoundComponent.SoundEvent("SOUND_TRUNK_OPEN");
+			}
+			else
+			{
+				m_SoundComponent.SoundEvent("SOUND_TRUNK_CLOSE");
+			}
+		}
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override bool GetActionNameScript(out string outName)
+	{
+		if((m_SignalsManager.GetSignalValue(m_iSignalIndex)) == 0)
+		{
+			outName = "Open";
+		} else
+		{
+			outName = "Close";
+		}
+		return true;
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override void Init(IEntity pOwnerEntity, GenericComponent pManagerComponent)
+	{
+		m_SignalsManager = SignalsManagerComponent.Cast(pOwnerEntity.FindComponent(SignalsManagerComponent));
+		m_SoundComponent = SoundComponent.Cast(pOwnerEntity.FindComponent(SoundComponent));
+	}
+}
