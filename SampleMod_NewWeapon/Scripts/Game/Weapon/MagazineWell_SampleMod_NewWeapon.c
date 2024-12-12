19ed23fb408fa24800a99c0de452192b4410af47
unknown <a4bldr@BLD-AR-W1-CZUPC.msad.bistudio.com>
2024-12-12 09:16:05 -0800

    Changed: Removed old skeleton in blend files

--- SampleMod_NewWeapon/Scripts/Game/Weapon/MagazineWell_SampleMod_NewWeapon.c
+++
@@ -1,7 +0,0 @@
-// Script File
-class MagazineWell65x39C_SampleWeaponClass {}
-MagazineWell65x39C_SampleWeaponClass MagazineWell65x39C_SampleWeaponSource;
-
-class MagazineWell65x39C_SampleWeapon : BaseMagazineWell
-{
-}
