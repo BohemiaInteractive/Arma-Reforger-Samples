07168a1e2c4a4c0d040625f441879ec1f287163e
unknown <a4bldr@BLD-AR-W1-CZUPC.msad.bistudio.com>
2024-12-12 02:09:14 -0800

    Added: Sample suppressor
    Changed: Made Sample Vest more future proof by preparing its SCR_ArmorDamageManagerComponent to be merged later with component, that will be added to parent prefab
    
    Changed: Removed old skeleton in blend files and replaced it with the new one
    
    Changed: Added air intake point to Sample New Car engine configuration

---
+++ SampleMod_NewWeapon/Scripts/Game/Weapon/SampleMod_NewWeapon_Attachments_muzzle.c
@@ -0,0 +1,5 @@
+class AttachmentMuzzle65_39Class {}
+AttachmentMuzzle65_39Class AttachmentMuzzle65_39Source;
+class AttachmentMuzzle65_39 : AttachmentMuzzle
+{
+};
---
+++ SampleMod_NewWeapon/Scripts/Game/Weapon/SampleMod_NewWeapon_Attachments_optics.c
@@ -0,0 +1,12 @@
+class AttachmentOpticsRIS1913IronsightFrontClass {}
+AttachmentOpticsRIS1913IronsightFrontClass AttachmentOpticsRIS1913IronsightFrontSource;
+class AttachmentOpticsRIS1913IronsightFront : AttachmentRIS1913
+{
+};
+
+
+class AttachmentOpticsRIS1913IronsightRearClass {}
+AttachmentOpticsRIS1913IronsightRearClass AttachmentOpticsRIS1913IronsightRearSource;
+class AttachmentOpticsRIS1913IronsightRear : AttachmentRIS1913
+{
+};
